45546e4d38fd96336d6b917a1a0895fcf636f16d
PythonToGo <pythontogoplease@gmail.com>
2024-08-08 22:46:18 +0200

    seperate password

--- api/index.py
+++ api/index.py
@@ -3,27 +3,44 @@ import sqlite3
 from datetime import datetime, timedelta
 import os
 import logging
+from dotenv import load_dotenv
 
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # 세션을 위한 시크릿 키 설정
+# current/template directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+template_dir = os.path.join(current_dir, '../templates')
 
-# 로깅 설정
+# .env load
+load_dotenv(os.path.join(current_dir, '../.env'))
+
+# initialize Flask 
+app = Flask(__name__, template_folder=template_dir)
+app.secret_key = os.getenv('SECRET_KEY') 
+
+# check env variables
+print(f"SECRET_KEY: {os.getenv('SECRET_KEY')}")
+print(f"LOGIN_PASSWORD: {os.getenv('LOGIN_PASSWORD')}")
+
+# logging
 logging.basicConfig(level=logging.INFO)
 
-# 데이터베이스 초기화
+# db initialize
 def init_db():
-    db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'schedule.db')
-    with sqlite3.connect(db_path) as conn:
-        conn.execute('''
-        CREATE TABLE IF NOT EXISTS schedule (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            week INTEGER,
-            day TEXT,
-            status TEXT
-        )''')
-        conn.commit()
+    try:
+        db_path = os.path.join(current_dir, 'schedule.db')
+        with sqlite3.connect(db_path) as conn:
+            conn.execute('''
+            CREATE TABLE IF NOT EXISTS schedule (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                week INTEGER,
+                day TEXT,
+                status TEXT
+            )''')
+            conn.commit()
+        app.logger.info("Database initialized successfully")
+    except Exception as e:
+        app.logger.error(f"Error initializing database: {e}")
 
-# 날짜 생성 함수
+# dates generator
 def generate_dates():
     today = datetime.today()
     start_this_week = today - timedelta(days=today.weekday())
@@ -34,11 +51,11 @@ def generate_dates():
 
     return this_week_dates, next_week_dates
 
-# 홈페이지 라우트
+# index route
 @app.route('/')
 def index():
     try:
-        db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'schedule.db')
+        db_path = os.path.join(current_dir, 'schedule.db')
         with sqlite3.connect(db_path) as conn:
             cursor = conn.cursor()
             cursor.execute('SELECT week, status FROM schedule ORDER BY week, id')
@@ -59,7 +76,8 @@ def index():
 def login():
     if request.method == 'POST':
         password = request.form['password']
-        if password == 'your_password':  # 올바른 비밀번호를 설정
+        login_password = os.getenv('LOGIN_PASSWORD')
+        if password == login_password:
             session['logged_in'] = True
             return redirect(url_for('update'))
         else:
@@ -67,7 +85,7 @@ def login():
             return render_template('login.html', error=error)
     return render_template('login.html')
 
-# 일정 입력 라우트
+# update route
 @app.route('/update', methods=['GET', 'POST'])
 def update():
     if not session.get('logged_in'):
@@ -83,7 +101,7 @@ def update():
                 schedule.append((0, day, request.form[f'this_week_{day}']))
                 schedule.append((1, day, request.form[f'next_week_{day}']))
 
-            db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'schedule.db')
+            db_path = os.path.join(current_dir, 'schedule.db')
             with sqlite3.connect(db_path) as conn:
                 conn.execute('DELETE FROM schedule')
                 conn.executemany('INSERT INTO schedule (week, day, status) VALUES (?, ?, ?)', schedule)
@@ -99,6 +117,10 @@ def handler(request, context):
     init_db()  # Initialize the database
     return app
 
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+# # internal server TEST
+# if __name__ == '__main__':
+#     init_db()
+#     app.run(host='127.0.0.1', port=8000, debug=True)
+
+# init_db() initialization
+init_db()
--- createqr.py
+++ createqr.py
@@ -2,7 +2,8 @@ import qrcode
 import os
 
 # URL for QR code
-url = 'https://qr-work-schedule.vercel.app?show_update=false'  # Vercel에서 제공된 URL로 변경
+# url = 'https://qr-plan.vercel.app?show_update=false' 
+url = 'https://127.0.0.1:8000?show_update=false' 
 
 # Create QR code
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
@@ -16,5 +17,3 @@ output_dir = os.path.dirname(os.path.abspath(__file__))
 output_file = os.path.join(output_dir, 'qrcode.png')
 
 img.save(output_file)
-
-print(f"QR 코드가 생성되었습니다. '{output_file}' 파일을 확인하세요.")
