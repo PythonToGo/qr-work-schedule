b149fb980d8c9e0a0dad9e8ef7015c6e97100bbc
PythonToGo <pythontogoplease@gmail.com>
2024-08-08 21:49:26 +0200

    deploy error fixed 2

--- createqr.py
+++ createqr.py
@@ -1,18 +1,20 @@
 import qrcode
 import os
 
-# url for QR code
-url = 'http://127.0.0.1:8000?show_update=false'
+# URL for QR code
+url = 'https://qr-work-schedule.vercel.app?show_update=false'  # Vercel에서 제공된 URL로 변경
 
-# create QR code
+# Create QR code
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
 qr.add_data(url)
 qr.make(fit=True)
 
-# save
+# Save
 img = qr.make_image(fill='black', back_color='white')
 
 output_dir = os.path.dirname(os.path.abspath(__file__))
 output_file = os.path.join(output_dir, 'qrcode.png')
 
 img.save(output_file)
+
+print(f"QR 코드가 생성되었습니다. '{output_file}' 파일을 확인하세요.")
